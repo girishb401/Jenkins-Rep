c64d25a03185b91696c3ceca8d9fa863e36107f6
girishb401 <39201100+girishb401@users.noreply.github.com>
2018-08-07 10:45:56 +0530

    Update ec2-start.py

--- ec2-start.py
+++ ec2-start.py
@@ -7,8 +7,8 @@ instance2 = ec2.Instance('i-038ed8dcd5fd39518')
 
 #print(instance.report_status())
 
-instance1.stop()
-instance2.stop()
+instance1.start()
+instance2.start()
 
 #print (report_status())
 
