da7bc1e6b3b214109106843e2c23bbac82936d05
Girish <girishb401@gmail.com>
2018-08-07 10:04:50 +0530

    fix for python

---
+++ ec2-start.py
@@ -0,0 +1,15 @@
+import boto3
+
+ec2 = boto3.resource('ec2')
+instance1 = ec2.Instance('i-00387bc334f900398')
+instance2 = ec2.Instance('i-038ed8dcd5fd39518')
+
+
+#print(instance.report_status())
+
+instance1.stop()
+instance2.stop()
+
+#print (report_status())
+
+
